0598655b5a52b94fe0e263c0b4123c74e9e919d7
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-07-18 19:29:23 +0200

    updated login and home components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  firebaseService = inject(FirebaseService);
+  authService = inject(AuthService);
+  router = inject(Router);
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+  });
 
+  async login() {
+    const name = this.profileForm.controls.name.value;
+    const userData = {
+      name: name,
+    };
+    const userId = await this.firebaseService.addDocument('users', userData);
+    this.authService.login(userData.name);
+    this.router.navigate(['/search', userId]);
+  }
 }
