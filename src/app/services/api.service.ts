9864a705a5b8c4bbeb047b052ac72793e2f256c5
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-07-18 21:59:41 +0200

    created apiService

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private API_KEY = '9065af0c';
+
+  async fetchResults(searchTerm: string) {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${this.API_KEY}&s=${encodeURIComponent(
+        searchTerm
+      )}`
+    );
+    const data = await response.json();
+    console.log(`API Antwort:`, data);
+    if (data.Response === 'True' && data.Search && data.Search.length > 0) {
+      return data.Search;
+    }
+    return [];
+  }
+}
