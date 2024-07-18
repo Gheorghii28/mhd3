eec202e6fc215d82c0d5ab24ba7b633fad1ade62
Gheorghii28 <popovici.gheorghii89@gmail.com>
2024-07-18 17:38:28 +0200

    Initial project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { CardFormComponent } from './home/card-form/card-form.component';
+import { ResultsComponent } from './home/results/results.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: '',
+        component: HomeComponent,
+        canActivate: [authGuard],
+        children: [
+          { path: 'search/:id', component: SearchComponent },
+          { path: 'card-form/:id', component: CardFormComponent },
+          { path: 'results', component: ResultsComponent },
+        ],
+      },
+      { path: 'login', component: LoginComponent },
+      { path: '**', redirectTo: '' },
+];
